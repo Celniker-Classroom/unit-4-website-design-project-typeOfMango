29588b2574a5ffa66d4ec0616a32ff93a1b331c5
typeOfMango <23davewood@gmail.com>
2026-04-13 16:24:23 +0000

    rgjsrjkfjlshfjlhalfweqf

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // add javascript here
+function myFunction(){
+    let dots = document.getElementById("dots");
+    let moreText  = document.getElementById("more");
+    let btnText = document.getElementById("myBtn");
+    if(dots.style.display === "none"){
+        dots.style.display = "inline";
+        btnText.innerHTML = "Show more";
+        moreText.style.display = "none";
+    }
+    else{
+        dots.style.display = "none";
+        btnText.innerHTML = "Show less";
+        moreText.style.display = "inline";
+    }
+}
