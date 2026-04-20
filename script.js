526639ce47c02f558704637405b00f3dcef58e95
typeOfMango <23davewood@gmail.com>
2026-04-20 16:16:51 +0000

    added some of a button

--- script.js
+++ script.js
@@ -14,7 +14,21 @@ function showMore(){
         moreText.style.display = "inline";
     }
 }
-
+function showEvenMore(){
+    let dots = document.getElementById("Dots");
+    let moreText = document.getElementById("More");
+    let btnText = document.getElementById("showEvenMore");
+    if(dots.style.display === "none"){
+        dots.style.display = "inline";
+        btnText.innerHTML = "Show More";
+        moreText.style.display = "none";
+    }
+    else{
+        dots.style.display = "none";
+        btnText.innerHTML = "show less";
+        moreText.style.display = "inline";
+    }
+}
 let id = null;
 function Animate() {
     let elem = document.getElementById("animation");
